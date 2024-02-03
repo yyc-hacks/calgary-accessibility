ccb61ce067a7662cea1dde6046c7a024cb1d8661
Shad Sajid <88870507+s-sajid@users.noreply.github.com>
2024-02-03 15:33:00 -0700

    Add phone number and fax number to schema

--- back-end/src/models/DoctorModel.ts
+++ back-end/src/models/DoctorModel.ts
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 const DoctorSchema = new mongoose.Schema({
 	name: { type: String, required: true },
 	location: { type: String, required: true },
+	phone_number: { type: String, required: false},
+	fax_number: { type: String, required: false},
 	practice_discipline: { type: [String], required: true },
 	gender: { type: String, required: true },
 	languages: { type: [String], required: true },

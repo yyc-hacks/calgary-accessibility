867fa341f323d3b2cf903dc3420a5c33efdfc104
Shad Sajid <88870507+s-sajid@users.noreply.github.com>
2024-02-03 15:18:08 -0700

    Add accepting_pateients to doctor model

--- back-end/src/models/DoctorModel.ts
+++ back-end/src/models/DoctorModel.ts
@@ -3,9 +3,10 @@ import mongoose from "mongoose";
 const DoctorSchema = new mongoose.Schema({
 	name: { type: String, required: true },
 	location: { type: String, required: true },
-	practice_discipline: { type: String, required: true },
+	practice_discipline: { type: [String], required: true },
 	gender: { type: String, required: true },
 	languages: { type: [String], required: true },
+	accepting_patients: { type: Boolean, default: false } 
 });
 
 const DoctorModel = mongoose.model("Doctor", DoctorSchema);
